c27ae9951d51bdb5749dcb400488d3c5ac640360
Daniel Nachun <daniel.nachun@gmail.com>
2025-09-30 13:52:19 -0700

    try to fix linking issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
+
+# Add rpath for pgconftest
+sed -i 's/-L${PG_LIBDIR}/-Wl,-rpath,${PG_LIBDIR} -L${PG_LIBDIR}/' configure
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
